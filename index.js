b2e63cb16740d2a34f295816332eeafb0207cd1d
AbdullahSharif <abdullah.29.umar@gmail.com>
2022-12-21 15:24:52 +0500

    body-parser, cors setup and database connection setup

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URI = "mongodb://localhost:27017";
+const PORT = process.env.PORT || 5000;
+
+// now we connect to the database.
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("Connected to mongoDB")).catch(() => console.log("Unable to connect to MongoDB"));
+mongoose.set('strictQuery', true);
+app.listen(PORT);
+console.log(`Listening on port : ${PORT}`);
